4b22d6bfc0b2bca670c447f5b6c68b029914f9ae
Kaija Gahm <kgahm@ucla.edu>
2023-01-04 16:13:01 -0800

    computation speed test. No particularly revolutionary results

--- dbscan_speedTest.R
+++ dbscan_speedTest.R
@@ -4,88 +4,52 @@
 
 library(tidyverse)
 library(fpc) # for DBSCAN clustering
-
-
+library(sf)
 
 # Load data
 load("data/simplifiedRoosts.Rda")
+load("data/distanceMatrix.Rda")
+distanceMatrix <- as.matrix(distanceMatrix)
+N <- nrow(distanceMatrix)
 
-# 1000m, 3pts, all data
-# 1000m, 3pts, 10% data
-# 1000m, 3pts, 25% data
-# 1000m, 3pts, 50% data
-# 1000m, 3pts, 75% data
-# 1000m, 3pts, 90% data
+# Make a list to hold the data and its percentages
+pcts <- c(1, .1, .25, .5, .75)
+whiches <- map(pcts, ~sample(1:N, size = round(N*.x), replace = F))
+pctList <- map(whiches, ~distanceMatrix[.x, .x])
 
-# eps is the reachability distance. I'm going to consider that to be 500m for starters. minPts for a core point is going to be 3.
-dbscan <- dbscan(distanceMatrix, eps = 750, method = "dist", MinPts = 3)
-
-data_dbscan <- bind_cols(testDF, "cluster" = dbscan$cluster) %>%
-  mutate(isOutlier = case_when(cluster == 0 ~ T,
-                               TRUE ~ F))
-
-# plot
-data_dbscan %>%
-  ggplot(aes(x = location_long, y = location_lat, col = factor(cluster)))+
-  geom_point(alpha = 0.3, aes(shape = isOutlier))+
-  scale_shape_manual(values = c(19, 3))+
-  theme_classic()+
-  theme(legend.position = "none")+
-  coord_equal()
-
-# zoom in
-data_dbscan %>%
-  filter(location_lat < 31.8, location_lat > 30.5, location_long < 35.4, location_long > 34.6) %>%
-  ggplot(aes(x = location_long, y = location_lat, col = factor(cluster)))+
-  geom_point(alpha = 0.3, aes(shape = isOutlier))+
-  scale_shape_manual(values = c(19, 3))+
-  theme_classic()+
-  theme(legend.position = "none")+
-  coord_equal()
-
-
-How many points are classified as outliers?
-  
-  
-nPoints <- nrow(testDF)
-dists <- c(100, 250, 500, 750, 1000, 1500)
-minpts <- 3
-dbscans_list <- vector(mode = "list", length = length(dists))
-for(i in 1:length(dists)){
-  dbscans_list[[i]] <- fpc::dbscan(distanceMatrix, eps = dists[i], method = "dist", MinPts = minpts)
+# Run dbscan as a loop so I can evaluate the runtime
+starts <- rep(NA, length(pcts))
+ends <- rep(NA, length(pcts))
+dbscans <- vector(mode = "list", length = length(pcts))
+pts <- 3
+eps <- 1000
+for(i in 1:length(pctList)){
+  start <- Sys.time()
+  out <- dbscan(pctList[[i]], eps = eps, method = "dist", MinPts = pts)
+  end <- Sys.time()
+  starts[[i]] <- start
+  ends[[i]] <- end
+  dbscans[[i]] <- out
+  cat(paste("Round", i, "done!"))
 }
-data_dbscans <- map(dbscans_list, ~bind_cols(testDF, "cluster" = .x$cluster) %>%
-                      mutate(isOutlier = ifelse(cluster == 0, T, F)))
-nOutliers <- map_dbl(data_dbscans, ~.x %>%
-                       filter(cluster == 0) %>%
-                       nrow())
-outliers <- data.frame(dist = dists, 
-                       nOutliers = nOutliers,
-                       propOutliers = nOutliers/nPoints)
+durations <- (ends-starts)/60
 
-outliers %>%
-  ggplot(aes(x = dist, y = propOutliers))+
-  geom_point(size = 2)+
+# Make a plot to see how long that took. Note to self: it actually didn't take nearly as long as I thought it would! Seems like computing the distance matrix was really the most difficult/time consuming step.
+dat <- data.frame(pct = pcts,
+                  nPts = map_dbl(whiches, length),
+                  duration = durations) %>%
+  mutate(nDists = nPts^2)
+
+dat %>%
+  ggplot(aes(x = nPts, y = duration))+
+  geom_point()+
   geom_line()+
-  theme_classic()+
-  ylab("Proportion of points not included in a cluster")+
-  xlab("Neighborhood distance (m)")
+  theme_classic()
 
-Make zoomed-in plots for each dbscan distance
-
-set.seed(1)
-zoomPlots <- map2(data_dbscans, dists, ~{
-  yr <- min(lubridate::year(.x$roost_date))
-  .x %>%
-    filter(location_lat < 31.8, location_lat > 30.5, location_long < 35.4, location_long > 34.6) %>%
-    ggplot(aes(x = location_long, y = location_lat, col = factor(cluster)))+
-    geom_point(alpha = 0.3, aes(shape = isOutlier))+
-    scale_shape_manual(values = c(19, 3))+
-    theme_classic()+
-    theme(legend.position = "none")+
-    coord_equal()+
-    ggtitle(paste0("Year = ", yr, "; MinPts = ", minpts, "; eps = ", .y, "m"))+
-    scale_color_paletteer_d("trekcolors::lcars_cardassian")
-})
-
-walk(zoomPlots, print)
+dat %>%
+  ggplot(aes(x = nDists, y = duration))+
+  geom_point()+
+  geom_line()+
+  theme_classic()
+# Hooray, I have discovered that these computations scale exponentially. DUH. The good news is, the runtime isn't actually that long (a couple of minutes! Very tractable!) even for quite a lot of data.
+# The rate-limiting step is undoubtedly going to be the distance matrix computation.
